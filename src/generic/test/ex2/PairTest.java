97fd9c7acd9e6386d078d554f05d47736547de6a
홍솔빈 <hongsolbin@hongsolbin-ui-MacBookPro-2.local>
2024-12-12 16:23:49 +0900

    제네릭문제풀이

---
+++ src/generic/test/ex2/PairTest.java
@@ -0,0 +1,19 @@
+package generic.test.ex2;
+
+public class PairTest {
+    public static void main(String[] args) {
+        Pair<Integer, String> p1 = new Pair<>();
+        p1.setFirst(1);
+        p1.setSecond("data");
+        System.out.println(p1.getFirst());
+        System.out.println(p1.getSecond());
+        System.out.println("p1 = " + p1);
+
+        Pair<String,String> p2 = new Pair<>();
+        p2.setFirst("key");
+        p2.setSecond("value");
+        System.out.println(p2.getFirst());
+        System.out.println(p2.getSecond());
+        System.out.println("p2 = " + p2);
+    }
+}
