257ff7c2a03f3964463a32bd54ac48d8f340d09d
solbin23 <solbin23@naver.com>
2024-12-17 22:30:21 +0900

    제네릭을 사용해 타입안정성을 보장해 주는 코드 추가

---
+++ src/collection/array/MyArrayListV4Main.java
@@ -0,0 +1,24 @@
+package collection.array;
+
+public class MyArrayListV4Main {
+
+    public static void main(String[] args) {
+
+        MyArrayListV4<String> stringList = new MyArrayListV4<>();
+        stringList.add("a");
+        stringList.add("b");
+        stringList.add("c");
+        stringList.add("d");
+
+        String string = stringList.get(0);
+        System.out.println("string = " + string);
+
+        MyArrayListV4<Integer> integerList = new MyArrayListV4<>();
+        integerList.add(1);
+        integerList.add(2);
+        integerList.add(3);
+
+        Integer integer = integerList.get(0);
+        System.out.println("integer = " + integer);
+    }
+}
