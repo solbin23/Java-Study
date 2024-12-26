eb151731653e903342d19b7cc2861b9edbcb5976
solbin23 <solbin23@naver.com>
2024-12-26 20:04:35 +0900

    문제 풀이 - 중복제거

---
+++ src/collection/set/test/UniqueNamesTest1.java
@@ -0,0 +1,21 @@
+package collection.set.test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UniqueNamesTest1 {
+
+    public static void main(String[] args) {
+
+        Integer[] inputArr = {30, 20, 20, 10, 10};
+        Set<Integer> set = new HashSet<>(); //O(1)
+        for (Integer i : inputArr) {
+            set.add(i);
+        }
+
+        for (Integer i : set) {
+            System.out.println(i);
+        }
+    }
+
+}
