2aff4d8656d0faab028f0d9727ea58eebe1fdd45
solbin23 <solbin23@naver.com>
2024-12-31 22:37:57 +0900

    Map을 활용한 문제풀이

---
+++ src/collection/map/test/WordFrequencyTest2.java
@@ -0,0 +1,24 @@
+package collection.map.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordFrequencyTest2 {
+
+    public static void main(String[] args) {
+        String text = "orange banana apple apple banana apple";
+
+        Map<String, Integer> map = new HashMap<>();
+
+        //코드 작성
+        String[] words = text.split(" ");
+        for (String word : words) {
+
+            Integer count = map.getOrDefault(word, 0);
+            count++;
+            map.put(word, count);
+        }
+
+        System.out.println(map);
+    }
+}
